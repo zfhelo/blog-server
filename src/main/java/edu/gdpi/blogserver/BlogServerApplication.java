b0fb3b8ddb73ee7c6b29cf08e06234350ceccdf7
zfhelo <zfhelo@gmail.com>
2020-11-20 20:50:22 +0800

    init

---
+++ src/main/java/edu/gdpi/blogserver/BlogServerApplication.java
@@ -0,0 +1,16 @@
+package edu.gdpi.blogserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ZhengHaiFeng
+ */
+@SpringBootApplication
+public class BlogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogServerApplication.class, args);
+    }
+
+}
